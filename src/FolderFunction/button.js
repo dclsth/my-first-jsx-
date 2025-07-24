d923026bc30310910f790a6c5d29d1f4cf672b8f
dclsth <calisthadara009@gmail.com>
2025-07-24 14:59:07 +0700

    Add files via upload

---
+++ src/FolderFunction/button.js
@@ -0,0 +1,14 @@
+import style from "styled-components";
+
+export const Button = style.button` 
+
+    width: 100px;
+    height: 100px;
+    background-color: #e891bcff; 
+    cursor: pointer;
+
+    &:hover {
+       background-color: pink; 
+      }
+       
+`
